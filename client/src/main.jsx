84446a0eabf22a2199ebce307a233209b61c9866
Tabish <mohdtabishkhan001@gmail.com>
2024-09-24 17:33:21 +0530

    implemented router and app is landing page now

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,26 @@
- 
+
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Home from './pages/Home.jsx'
 import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import Map from './components/Map.jsx'
+import './index.css'
 import store from './store/store.js'
+const router = createBrowserRouter(
+  [
+
+    {
+      path: "/",
+      element: <App />
+    },
+    {
+      path: "/map",
+      element: <Map />
+    }
+  ]
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <Home />
-    <App />
+    <RouterProvider router={router} />
   </Provider>,
 )
