3437e8a2288de45f09ebd97c128b873c8e3035d5
Tabish <mohdtabishkhan001@gmail.com>
2024-09-29 00:51:35 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     },
     build: {
       rollupOptions: {
-        external: ['react-redux'],
+        external: ['react-redux', 'react-router-dom'],
       },
     }
   },
