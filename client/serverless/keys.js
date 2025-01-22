9a09297366bfd3caabd6b59135ee3e6a7a9f5907
Tabish <mohdtabishkhan001@gmail.com>
2025-01-22 21:30:45 +0530

    .

--- client/serverless/keys.js
+++ client/serverless/keys.js
@@ -1,7 +1,6 @@
 export default function keys() {
-    // const gemini_api_key = "AIzaSyAGhyWAfD4bckDqZgs6eYeImMOeFeNSfjY"
     const gemini_api_key = ""
     const connection_url = ""
     return { gemini_api_key, connection_url }
-     
+
 }
