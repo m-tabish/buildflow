757e2390c01f96a1b68d59e3a40011e1b30ad927
Tabish <mohdtabishkhan001@gmail.com>
2025-01-22 21:27:13 +0530

    removed

--- client/serverless/keys.js
+++ client/serverless/keys.js
@@ -1,6 +1,7 @@
 export default function keys() {
     // const gemini_api_key = "AIzaSyAGhyWAfD4bckDqZgs6eYeImMOeFeNSfjY"
-    const gemini_api_key = "AIzaSyBmhPXDONfLfhMzykx8vDsTXlJw_lnV5JY"
-    const connection_url = "mongodb+srv://mohdtabishkhan001:hOhJPMDpp5PVA8pQ@cluster0.kzbam.mongodb.net/"
+    const gemini_api_key = ""
+    const connection_url = ""
     return { gemini_api_key, connection_url }
+     
 }
